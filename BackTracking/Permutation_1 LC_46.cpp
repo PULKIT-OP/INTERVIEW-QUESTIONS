d53a1f394af7558adac4937762d4733b8b63e810
PULKIT <131228343+PULKIT-OP@users.noreply.github.com>
2025-08-26 19:03:56 +0530

    Update Permutation_1 LC_46.cpp

--- BackTracking/Permutation_1 LC_46.cpp
+++ BackTracking/Permutation_1 LC_46.cpp
@@ -1,1 +1,34 @@
+// BackTracking approach
+class Solution {
+public:
+    int n;
+    vector<vector<int>> result;
+    unordered_set<int> st;
+    void solve(vector<int> &temp, vector<int> &nums){
+        if(temp.size() == n){
+            result.push_back(temp);
+            return;
+        }
 
+        for(int i = 0; i < n; i++){
+            if(st.find(nums[i]) == st.end()){
+              // take a step
+                temp.push_back(nums[i]);
+                st.insert(nums[i]);
+              // explore the step
+                solve(temp, nums);
+              // reverse the step
+                temp.pop_back();
+                st.erase(nums[i]);
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        n = nums.size();
+      
+        vector<int> temp;
+        solve(temp, nums);
+      
+        return result;
+    }
+};
